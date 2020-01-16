8619063ad98ab66ce826047d5ef4d6aed5ba96e2
Renniej <tai.juan2@gmail.com>
2020-01-15 21:54:26 -0500

    Data services finished :P

--- A1/A1-Web_API/data-services.js
+++ A1/A1-Web_API/data-services.js
@@ -5,56 +5,128 @@ const mongoose = require('mongoose')
 var exports = module.exports = {};
 var Schema = mongoose.Schema;
 
-var Car = mongoose.model('Car', 
-new Schema({ 
-    
-    Car_Brand: String, 
-    Car_Model: String, 
-    Car_Year: Number,
-    Car_VIN : String,
-    CAR_MSRP : Number,
-    Car_Photo : String,
+var Car = mongoose.model('Car',
+    new Schema({
 
-    Car_Purchaser_Name : String,
-    Car_Purchase_Date : Date,
-    Car_Purachaser_Email : String,
-    Car_Purchaser_Price : Number,
-    Car_Purchaser_Photo : String
+        Car_Brand: String,
+        Car_Model: String,
+        Car_Year: Number,
+        Car_VIN: String,
+        CAR_MSRP: Number,
+        Car_Photo: String,
 
-
-}), 
-'MOCK_DATA');     // collection name
+        Car_Purchaser_Name: { type: String, default: null },
+        Car_Purchase_Date: { type: Date, default: null },
+        Car_Purachaser_Email: { type: String, default: null },
+        Car_Purchaser_Price: { type: Number, default: null },
+        Car_Purchaser_Photo: { type: String, default: null }
 
 
-exports.initializeDBConnection = function(){
+    }),
+    'MOCK_DATA');     // collection name
 
 
-    return new Promise(function(resolve, reject){
-        mongoose.connect('mongodb+srv://Rennie:tHmxIPK3KeX5FnYy@senecaweb-dojoo.mongodb.net/BTI425?retryWrites=true&w=majority',{useNewUrlParser : true, useUnifiedTopology : true})
-        
-        .then(function(data){
-            
-            resolve("Mongodb connection successful :)")
-            
-        }).catch(function(err){
-            
+exports.initializeDBConnection = function () {
+
+
+    return new Promise(function (resolve, reject) {
+        mongoose.connect('mongodb+srv://Rennie:tHmxIPK3KeX5FnYy@senecaweb-dojoo.mongodb.net/BTI425?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+
+            .then(function (data) {
+
+                resolve("Mongodb connection successful :)")
+
+            }).catch(function (err) {
+
                 reject(err);
-         });
+            });
     });
-} ;
+};
 
 
-exports.getAll = function(){
-    return new Promise(function(resolve, reject){
+exports.Find = function (id) {
+    console.log("FIND CALLED")
 
-        Car.find({}, function(err, data){
+    return new Promise(function (resolve, reject) {
+
+        if (id) { //if a condition is sent then find all documents with that condition
+
+            console.log(id);
+            Car.findById(id, function (err, data) {
+
+                if (err) reject(err)
+                else resolve(data);
+
+
+            })
+
+        }
+        else { //else grab all documents
+            console.log("LOOKING")
+            Car.find({}, function (err, data) {
+
+                if (err) reject(err)
+                else resolve(data);
+
+
+            })
+        }
+
+
+    })
+}
+
+exports.Add_New_Car = function (newCar) {
+
+    return new Promise(function (resolve, reject) {
+
+        Car.create(newCar, function (err, data) {
 
             if (err) reject(err)
             else resolve(data);
-
 
         })
 
 
     })
+
 }
+
+
+exports.Update_Car = function (newCar) {
+
+    return new Promise(function (resolve, reject) {
+
+        Car.findByIdAndUpdate(newCar._id, newCar, { new: true }, function (err, data) {
+
+            if (err) reject(err)
+            else resolve(data);
+
+        })
+
+
+    })
+
+
+}
+
+
+exports.Remove_Car = function (id) {
+
+    return new Promise(function (resolve, reject) {
+
+        Car.findByIdAndDelete(id, function (err) {
+
+            if (err) reject(err)
+            else resolve()
+
+
+        })
+
+
+
+    })
+
+
+}
+
--- A1/A1-Web_API/server.js
+++ A1/A1-Web_API/server.js
@@ -1,46 +1,64 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors") //WHAT IS CORS??????/
+const data_services = require("./data-services.js")
+
+
 var app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
 var HTTP_PORT = process.env.PORT || 8080; // http://localhost:8080/
-const data_services =  require("./data-services.js")
+
 
 function onHttpStart() {
 
     data_services.initializeDBConnection()
-    
-    .then(function(DB_msg){
 
-        console.log(DB_msg)
-        console.log("Express http server listening on: " + HTTP_PORT);
-    })
-    .catch(function(err){
-        console.log("MongoDb Error : " + err);
-    });
+        .then(function (DB_msg) {
+
+            console.log(DB_msg)
+            console.log("Express http server listening on: " + HTTP_PORT);
+        })
+        .catch(function (err) {
+            console.log("MongoDb Error : " + err);
+        });
 
 }
 
-app.get('/api/cars', function(req,res){ //get all car documents
+app.get('/api/cars', function (req, res) { //get all car documents
 
-    data_services.getAll().then(function(data){
+    data_services.Find().then(function (data) {
         var status;
 
-        if (data.length){
+        if (data.length) {
             status = 200; //Data didn't return as an empty query
         }
-        else{
+        else {
             status = 404; //Data returned is empty a.k.a data matching query was not found
         }
 
         res.status(status) //Sucessful response
         res.json(data)
     }
-    ).catch(function(err){
+    ).catch(function (err) {
         res.status(500) //Api error ):
         res.send("API ERROR : " + err);
     })
 
-app.get('/api/cars/:id', function(req,res){ //get single car document
 
-});
+})
+
+
+app.get('/api/cars/:id', function (req, res) { //get single car documents
+
+    data_services.Find(req.params.id).then(function (data) {
+        res.json(data)
+    }).catch(function (err) {
+        res.send("ERROR : " + err);
+    })
+
 
 })
 
